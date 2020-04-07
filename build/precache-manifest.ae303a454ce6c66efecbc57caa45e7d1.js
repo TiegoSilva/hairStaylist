4d9c3a29821956316d0f28511dab47975c623a3f
TiegoSilva <tiego.silvaa@gmail.com>
2020-04-07 17:25:37 -0300

    upload build

---
+++ build/precache-manifest.ae303a454ce6c66efecbc57caa45e7d1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1653b2af5e3e25cf23fb0b1b5c09cf74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40b77e2b5b677b7ebba7",
+    "url": "/static/css/main.ec5033c0.chunk.css"
+  },
+  {
+    "revision": "177d54c1024838112585",
+    "url": "/static/js/2.02e72c1b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.02e72c1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40b77e2b5b677b7ebba7",
+    "url": "/static/js/main.df11e9d9.chunk.js"
+  },
+  {
+    "revision": "48f71fe120e02acaeefd",
+    "url": "/static/js/runtime-main.9101ca26.js"
+  },
+  {
+    "revision": "f9b5142d6cb334f8e2307d6fb63bdb10",
+    "url": "/static/media/blonde.f9b5142d.png"
+  },
+  {
+    "revision": "e0653a40d52dceacc1b32ced3b3944d4",
+    "url": "/static/media/service-manicure.e0653a40.png"
+  },
+  {
+    "revision": "5661db01aeaf5b02695c06802c6f73d5",
+    "url": "/static/media/service-mechas.5661db01.png"
+  },
+  {
+    "revision": "25039cf63aa58eb0476981bbe1441ba6",
+    "url": "/static/media/service-sobrancelha.25039cf6.png"
+  }
+]);
