3eb6db7cc792e4b300aff0fa84b60fe806d7ba81
TiegoSilva <tiego.silvaa@gmail.com>
2020-04-07 17:15:30 -0300

    manifest.json edited to transform in a react app

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.04235e5d1b99ede137020d2a65544540.js"
+  "/precache-manifest.ae303a454ce6c66efecbc57caa45e7d1.js"
 );
 
 self.addEventListener('message', (event) => {
